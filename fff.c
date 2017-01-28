de30147b92869ead425630dd5a9f93a3a6b3f246
futurist <jamesyang999@gmail.com>
2017-01-28 12:04:38 +0800

    add: timeSpan support; formatted code

--- fff.c
+++ fff.c
@@ -1,8 +1,7 @@
-
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
 
-FILE* logFile;
+FILE *logFile;
 char bufferForKeys[100];
 
 BYTE keyState[256];
@@ -13,83 +12,90 @@ HANDLE singleInstanceMutexHandle;
 HHOOK keyboardHookHandle;
 HANDLE quitEventHandle;
 
-void LogToFile( char* data )
-{
-    fputs( data, logFile );
-    fflush( logFile );
-}
+typedef struct METASTRUCT {
+  int version;
+  DWORD startTime;
+} METASTRUCT;
 
-LRESULT CALLBACK KeyboardHookDelegate( int nCode, WPARAM wParam, LPARAM lParam )
-{
+DWORD prevTime = 0;
+DWORD prevVkCode = 0;
 
-	BOOL isDown = FALSE;
-  	BOOL isUp = FALSE;
+LRESULT CALLBACK KeyboardHookDelegate(int nCode, WPARAM wParam, LPARAM lParam) {
 
-    if (nCode == HC_ACTION)
-    {
-      PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
+  if (nCode == HC_ACTION) {
+    PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
 
-      isDown = wParam==WM_KEYDOWN || wParam==WM_SYSKEYDOWN;
-      isUp = wParam==WM_KEYUP || wParam==WM_SYSKEYUP;
-      GetKeyboardState((PBYTE)&keyState);
+    /* BOOL isDown = wParam==WM_KEYDOWN || wParam==WM_SYSKEYDOWN; */
+    /* BOOL isUp = wParam==WM_KEYUP || wParam==WM_SYSKEYUP; */
+    /* GetKeyboardState((PBYTE)&keyState); */
 
-      // fprintf(logFile, "%02x", isDown);
-      fwrite(p, sizeof(KBDLLHOOKSTRUCT), 1, logFile);
-      fflush( logFile );
-
-      //sprintf( bufferForKeys, "%d : %d : %X %X %X %X %X\n", GetTickCount(), isDown, p->vkCode, p->scanCode, p->flags, p->time, p->dwExtraInfo);
-      //LogToFile( bufferForKeys );
+    // don't record same key again
+    if (p->time == prevTime && p->vkCode == prevVkCode) {
+      return CallNextHookEx(NULL, nCode, wParam, lParam);
     }
 
-    return CallNextHookEx( NULL, nCode, wParam, lParam );
+    // fprintf(logFile, "%02x", isDown);
+    fwrite(p, sizeof(KBDLLHOOKSTRUCT), 1, logFile);
+    fflush(logFile);
+
+    prevTime = p->time;
+    prevVkCode = p->vkCode;
+  }
+
+  return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-DWORD WINAPI ThreadedCode( LPVOID )
-{
-    WaitForSingleObject( quitEventHandle, INFINITE );
-    
-    CloseHandle( singleInstanceMutexHandle );
-    CloseHandle( quitEventHandle );
-    UnhookWindowsHookEx( keyboardHookHandle );
-    fclose( logFile );
-    
-    ExitProcess( 0 );
-    
-    return 0;
+DWORD WINAPI ThreadedCode(LPVOID) {
+  WaitForSingleObject(quitEventHandle, INFINITE);
+
+  CloseHandle(singleInstanceMutexHandle);
+  CloseHandle(quitEventHandle);
+  UnhookWindowsHookEx(keyboardHookHandle);
+  fclose(logFile);
+
+  ExitProcess(0);
+
+  return 0;
 }
 
-int WINAPI WinMain( HINSTANCE currentInstance, HINSTANCE previousInstance, LPSTR commandLine, int showMode )
-{
-    const char singleInstanceMutexName[] = "EventRecorder";
-    const char quitEventName[] = "winlogon";
-    
-    singleInstanceMutexHandle = CreateMutex( NULL, TRUE, singleInstanceMutexName );
-    int isAlreadyRunning = GetLastError() == ERROR_ALREADY_EXISTS;
-        
-    if( !isAlreadyRunning )
-    {
-        
-        char logFilePath[MAX_PATH] = { 0 };
-        sprintf( logFilePath, "log.key" );
-        
-        logFile = fopen( logFilePath, "a" );
-        keyboardHookHandle = SetWindowsHookEx( WH_KEYBOARD_LL, KeyboardHookDelegate, currentInstance, 0 );
-        {
-            quitEventHandle = CreateEvent( NULL, FALSE, FALSE, quitEventName );
-            
-            int threadId;
-            CreateThread( NULL, 0, ThreadedCode, 0, 0, &threadId );
-            
-            MSG message;
-            while( GetMessage( &message, NULL, 0, 0 ) )
-            {}
-        }
-    }
-    else
-    {
-        quitEventHandle = OpenEvent( EVENT_ALL_ACCESS, FALSE, quitEventName );
-        SetEvent( quitEventHandle );
-    }
+int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance,
+                   LPSTR commandLine, int showMode) {
+  const char singleInstanceMutexName[] = "EventRecorder";
+  const char quitEventName[] = "winlogon";
 
-    return 0;
+  singleInstanceMutexHandle = CreateMutex(NULL, TRUE, singleInstanceMutexName);
+  int isAlreadyRunning = GetLastError() == ERROR_ALREADY_EXISTS;
+
+  if (!isAlreadyRunning) {
+
+    char logFilePath[MAX_PATH] = {0};
+    sprintf(logFilePath, "log.key");
+
+    METASTRUCT *meta = (METASTRUCT *)malloc(sizeof(METASTRUCT));
+
+    meta->version = 1;
+    meta->startTime = GetTickCount();
+
+    // write file header
+    logFile = fopen(logFilePath, "w");
+    fwrite(meta, sizeof(METASTRUCT), 1, logFile);
+
+    keyboardHookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookDelegate,
+                                          currentInstance, 0);
+    {
+      quitEventHandle = CreateEvent(NULL, FALSE, FALSE, quitEventName);
+
+      int threadId;
+      CreateThread(NULL, 0, ThreadedCode, 0, 0, &threadId);
+
+      MSG message;
+      while (GetMessage(&message, NULL, 0, 0)) {
+      }
+    }
+  } else {
+    quitEventHandle = OpenEvent(EVENT_ALL_ACCESS, FALSE, quitEventName);
+    SetEvent(quitEventHandle);
+  }
+
+  return 0;
 }
--- ppp.c
+++ ppp.c
@@ -1,46 +1,69 @@
-
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
 
 char strBuffer[100];
+DWORD prevTime = 0;
 
+typedef struct METASTRUCT {
+  int version;
+  DWORD startTime;
+} METASTRUCT;
 
-int WINAPI WinMain( HINSTANCE currentInstance, HINSTANCE previousInstance, LPSTR commandLine, int showMode )
-{
-   
-        FILE* logFile;
-        char logFilePath[MAX_PATH] = { 0 };
-        sprintf( logFilePath, "log.key" );
-        logFile = fopen( logFilePath, "r" );
+int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance,
+                   LPSTR commandLine, int showMode) {
 
+  FILE *logFile;
+  char logFilePath[MAX_PATH] = {0};
+  sprintf(logFilePath, "log.key");
+  logFile = fopen(logFilePath, "r");
 
-        FILE* logFile2;
-        char logFilePath2[MAX_PATH] = { 0 };
-        sprintf( logFilePath2, "log.txt" );
-        logFile2 = fopen( logFilePath2, "w" );
+  FILE *logFile2;
+  char logFilePath2[MAX_PATH] = {0};
+  sprintf(logFilePath2, "log.txt");
+  logFile2 = fopen(logFilePath2, "w");
 
+  if (logFile == NULL) {
+    perror("Error read input file");
+    return 1;
+  } else {
 
-    if (logFile==NULL) {
-        perror("Error read input file");
-        return 1;
+    int timeSpan = 0;
+    METASTRUCT *meta = (METASTRUCT *)malloc(sizeof(METASTRUCT));
+    KBDLLHOOKSTRUCT *keyRecord =
+        (KBDLLHOOKSTRUCT *)malloc(sizeof(KBDLLHOOKSTRUCT));
+
+    if (fread(meta, sizeof(METASTRUCT), 1, logFile) != 1) {
+      perror("Error read header");
+      return 2;
     }
-    else {
+    prevTime = meta->startTime;
+    fprintf(logFile2, "time: %d\n", meta->startTime);
 
-        KBDLLHOOKSTRUCT *p= (KBDLLHOOKSTRUCT*)malloc(sizeof(KBDLLHOOKSTRUCT));
-        size_t size = sizeof(KBDLLHOOKSTRUCT);
+    while (!feof(logFile) &&
+           fread(keyRecord, sizeof(KBDLLHOOKSTRUCT), 1, logFile) == 1) {
 
-        while( !feof(logFile) && fread(p, size, 1, logFile) == 1 ) {
-                         
-            sprintf( strBuffer, " %d\n", 1);
-            fputs( strBuffer, logFile2 );
+      if (prevTime) {
 
-            keybd_event(p->vkCode, p->scanCode, p->flags, p->dwExtraInfo);
-
+        timeSpan = keyRecord->time - prevTime;
+        if (timeSpan < 0) {
+          perror("Error time record");
+          return 3;
         }
+        if (timeSpan > 0)
+          sleep(timeSpan);
+
+        BOOL isExtended = keyRecord->flags & LLKHF_EXTENDED ? KEYEVENTF_EXTENDEDKEY : 0;
+        BOOL isUp = keyRecord->flags & LLKHF_UP ? KEYEVENTF_KEYUP : 0;
+        fprintf(logFile2, " %c %X %d %X %X\n", keyRecord->vkCode, keyRecord->flags, keyRecord->time, isExtended, isUp);
+        keybd_event(keyRecord->vkCode, keyRecord->scanCode, isExtended | isUp,
+                    keyRecord->dwExtraInfo);
+      }
+      prevTime = keyRecord->time;
     }
+  }
 
-fclose( logFile );
-fclose( logFile2 );
+  fclose(logFile);
+  fclose(logFile2);
 
-    return 0;
+  return 0;
 }
